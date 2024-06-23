55f406a65c0091c04caf013b90cd55ee4fab8a89
Bia33 <89039688+Bianca3489@users.noreply.github.com>
2024-06-23 17:28:37 -0300

    meu primeiro commit

---
+++ main.py
@@ -0,0 +1,22 @@
+
+print("Projeto de dados")
+
+# Variaveis
+
+idade = 34
+nome = "Bianca"
+
+# Print
+print(idade)
+
+# Soma
+nova_idade = idade + 1
+print(nova_idade)
+
+# lista
+
+lista = [1, 2, 3, 4, 5]
+
+# Metodos
+
+lista_invertida = lista.reverse()
